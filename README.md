b4787ce8bb683e93b7c7ea2b5f35127f5d534fcd
Nicolas Neubauer <nneubauer@Nicolas-MacBook-Pro.local>
2018-06-05 22:38:50 +0200

    Changed so it seems to work with denon-client.

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
-# Homebridge-Denon
-homebridge plugin for denon/marantz receivers
+# A Plugin for hombridge to control a Denon AVR
+
+This has been forked from https://github.com/xkonni/homebridge-denon which unfortunatly uses a modified version of
+the node-denon-client and has little to no documentation.
+
+# Install
+
+The plugin registers as `hombridge-denon-avr` to avoid ambuigity with another plugin that already exists `homebridge-denon`,
+which I found not to work with newer Denon AVR generations. Until I find out how to publish note packages use
+
+`git clone`
+`cd homebridge-denon`
+`sudo npm -g install .`
 
 ## features
 * rudimentary support for zone1,zone2 via homebridge
@@ -8,6 +19,5 @@ homebridge plugin for denon/marantz receivers
 * defaultVolume not yet working
 
 ## dependencies
-* denon-client, modified: https://github.com/xkonni/node-denon-client
-  * based on https://github.com/lmoe/node-denon-client
-* this code itself is roughly based on https://github.com/stfnhmplr/homebridge-denon-marantz-avr
+
+Based on https://github.com/lmoe/node-denon-client
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = function (homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
 
-  homebridge.registerAccessory('homebridge-denon-client', 'DenonClient', denonClient);
+  homebridge.registerAccessory('homebridge-denon-avr', 'DenonAVR', denonClient);
 };
 
 function denonClient(log, config) {
@@ -15,105 +15,80 @@ function denonClient(log, config) {
   this.config = config;
   this.name = config['name'];
   this.ip = config['ip'];
-  this.zone = config['zone'] || 1;
-  // TODO not yet working
-  this.defaultVolume = config['defaultVolume'] || false;
-  this.defaultInput = config['defaultInput'] || false;
+
+  this.requiredInput = config['input'] || false;
   this.debug = config['debug'] || false;
 
-
-  this.avr = new Denon.DenonClient(this.ip);
-  this.avr.connect();
-  this.state = false;
-
   // state variables
-  this.volume = 0;
-  this.power = "OFF";
-  this.input = "N/A";
-  this.mute = false;
+  this.avr = new Denon.DenonClient(this.ip);
+  this.currentPowerState = 'OFF';
+  this.currentInput = '';
 
   // listeners
-  if (this.zone == 1) {
-    this.avr.on('masterVolumeChanged', (volume) => {
-      this.volume = volume;
-      console.log(`Volume changed to: ${volume}`);
+  this.avr.on('inputChanged', (newInput) => {
+    this.currentInput = newInput;
+    this.log(`Input changed to: ${newInput}`);
+  });
+  this.avr.on('powerChanged', (newPowerState) => {
+    this.currentPowerState = newPowerState;
+    this.log(`powerState changed to: ${newPowerState}`);
+  });
+
+  // inital values
+  this.avr.connect()
+    .then(() => {
+      this.log(`Tryin to get Input`);
+      return this.avr.getInput()
+    })
+    .then((result) => {
+      this.log(`Current input retrieved as: ${result}`);
+      this.currentInput = result;
+    })
+    .catch((error) => {
+      this.log('Error %s', error);
     });
-    this.avr.on('zone1Changed', (power) => {
-      this.power = power;
-      console.log(`Zone1 changed to: ${power}`);
+
+  this.avr.connect()
+    .then(() => {
+      this.log(`Tryin to get power state`);
+      return this.avr.getPower()
+    })
+    .then((result) => {
+      this.log(`Current power retrieved as: ${result}`);
+      this.currentPowerState = result;
+    })
+    .catch((error) => {
+      this.log('Error %s', error);
     });
-    this.avr.on('inputChanged', (input) => {
-      this.input = input;
-      console.log(`Input changed to: ${input}`);
-    });
-    this.avr.on('muteChanged', (mute) => {
-      this.mute = {mute};
-      console.log(`mute changed to: ${mute}`);
-    });
-  }
-  else {
-    this.avr.on('zone2Changed', (power) => {
-      this.power = power;
-      console.log(`Zone2 changed : ${power}`);
-    });
-  }
 }
 
 denonClient.prototype.getPowerState = function (callback, context) {
-  this.log('getPowerState Zone: %d Power: %s', this.zone, this.power);
-  callback(null,  (this.power == 'ON'));
+  this.log('getPowerState: %s', this.currentPowerState);
+  callback(null, (this.currentPowerState == 'ON'));
 };
 
 denonClient.prototype.setPowerState = function (powerState, callback, context) {
-  this.log('setPowerState Zone: %d to:  %s', this.zone, powerState);
-  if (this.zone == 1) {
-  this.avr.setZone1((powerState) ? 'ON' : 'OFF')
-    .then(() => {
-      if(powerState && this.defaultInput) {
-        this.log('setting defaultInput Zone: %d to: %s', this.zone, this.defaultInput);
-        return this.avr.setInput(this.defaultInput);
-      }
-    });
-  }
-  else {
-    this.avr.setZone2((powerState) ? 'ON' : 'OFF')
-    .then(() => {
-      if(powerState && this.defaultInput) {
-        this.log('setting defaultInput Zone: %d to: %s', this.zone, this.defaultInput);
-        return this.avr.setZone2(this.defaultInput);
-      }
-    });
-  }
+  this.log('setPowerState to: %s', powerState);
+  this.avr.connect()
+  .then(() => {
+    this.log('Connected.');
+    return this.avr.setPower((powerState) ? 'ON' : 'OFF');
+  })
+  .then(() => {
+    this.log('Powerstate has been set to %s', this.powerState);
+    this.currentPowerState = powerState;
 
-  // TODO: for some reason always sets volume to 0
-  // .then(() => {
-  //   if (powerState && this.defaultVolume) {
-  //     this.log('setting defaultVolume: %d', this.defaultVolume);
-  //     return this.avr.setVolume(parseInt(this.defaultVolume));
-  //   }
-  // });
+    if(powerState && this.currentInput != this.requiredInput) {
+      this.log('setting Input to %s', this.requiredInput);
+      return this.avr.setInput(this.requiredInput);
+    }
+  })
+  .catch((error) => {
+      // Oh noez.
+      this.log('Error %s', error);
+  });
+
   callback(null);
-};
-
-denonClient.prototype.getVolume = function (callback) {
-  this.log('getVolume: %d', this.volume);
-  callback(null, this.volume);
-};
-
-denonClient.prototype.setVolume = function (pVol, callback) {
-  this.avr.setVolume(pVol);
-  callback(null);
-};
-
-denonClient.prototype.setMuteState = function (state, callback) {
-  this.log('setMuteState: %s', state);
-  this.avr.setMute((state) ? 'ON' : 'OFF');
-  callback(null);
-};
-
-denonClient.prototype.getMuteState = function (callback) {
-  this.log('getMuteState: %s', this.mute);
-  callback(null, this.mute);
 };
 
 denonClient.prototype.getServices = function () {
@@ -123,21 +98,11 @@ denonClient.prototype.getServices = function () {
     .setCharacteristic(Characteristic.Name, this.name)
     .setCharacteristic(Characteristic.Manufacturer, this.type || 'denon');
 
-  // TODO Fan for now
-  this.homebridgeService = new Service.Fan(this.name);
+  // This is a switch that is on only if the avr is on && the input is correct.
+  this.homebridgeService = new Service.Switch(this.name);
   this.homebridgeService.getCharacteristic(Characteristic.On)
     .on('get', this.getPowerState.bind(this))
     .on('set', this.setPowerState.bind(this));
-
-  // this.homebridgeService.addCharacteristic(Characteristic.Volume)
-  this.homebridgeService.addCharacteristic(Characteristic.RotationSpeed)
-    .on('get', this.getVolume.bind(this))
-    .on('set', this.setVolume.bind(this));
-
-  // this.homebridgeService.addCharacteristic(Characteristic.Mute)
-  this.homebridgeService.addCharacteristic(Characteristic.RotationDirection)
-    .on('get', this.getMuteState.bind(this))
-    .on('set', this.setMuteState.bind(this));
 
   return [informationService, this.homebridgeService];
 };
