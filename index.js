da64a3ea0a1549b096b59b0a2ccac2c3f862240b
Nicolas Neubauer <nneubauer@Nicolas-MacBook-Pro.local>
2018-06-06 22:17:35 +0200

    A working version via telnet that is not multi accessory capable.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var Service, Characteristic;
-var Denon = require('denon-client');
+var Denon = require('marantz-denon-telnet');
 
 module.exports = function (homebridge) {
   Service = homebridge.hap.Service;
@@ -20,75 +20,70 @@ function denonClient(log, config) {
   this.debug = config['debug'] || false;
 
   // state variables
-  this.avr = new Denon.DenonClient(this.ip);
-  this.currentPowerState = 'OFF';
-  this.currentInput = '';
-
-  // listeners
-  this.avr.on('inputChanged', (newInput) => {
-    this.currentInput = newInput;
-    this.log(`Input changed to: ${newInput}`);
-  });
-  this.avr.on('powerChanged', (newPowerState) => {
-    this.currentPowerState = newPowerState;
-    this.log(`powerState changed to: ${newPowerState}`);
-  });
-
-  // inital values
-  this.avr.connect()
-    .then(() => {
-      this.log(`Tryin to get Input`);
-      return this.avr.getInput()
-    })
-    .then((result) => {
-      this.log(`Current input retrieved as: ${result}`);
-      this.currentInput = result;
-    })
-    .catch((error) => {
-      this.log('Error %s', error);
-    });
-
-  this.avr.connect()
-    .then(() => {
-      this.log(`Tryin to get power state`);
-      return this.avr.getPower()
-    })
-    .then((result) => {
-      this.log(`Current power retrieved as: ${result}`);
-      this.currentPowerState = result;
-    })
-    .catch((error) => {
-      this.log('Error %s', error);
-    });
+  this.avr = new Denon(this.ip);
+  
 }
 
 denonClient.prototype.getPowerState = function (callback, context) {
-  this.log('getPowerState: %s', this.currentPowerState);
-  callback(null, (this.currentPowerState == 'ON'));
+  this.log('getPowerState');
+  var that = this;
+
+  this.avr.getPowerState(function(error, powerState) {
+    if(error) {
+      that.log("Error while getting power state %s", error);
+    }
+
+    if(powerState) {
+      // if it is on, still have to check if input matches
+      that.avr.getInput(function(error, input) {
+        if(error) {
+          that.log("Error while getting input state %s", error);
+        }
+  
+        callback(null, powerState && input == that.requiredInput);
+      });
+    }
+    else {
+      //not powered on, means off no matter the input
+      callback(null, powerState);
+    }
+  });
 };
 
 denonClient.prototype.setPowerState = function (powerState, callback, context) {
   this.log('setPowerState to: %s', powerState);
-  this.avr.connect()
-  .then(() => {
-    this.log('Connected.');
-    return this.avr.setPower((powerState) ? 'ON' : 'OFF');
-  })
-  .then(() => {
-    this.log('Powerstate has been set to %s', this.powerState);
-    this.currentPowerState = powerState;
-
-    if(powerState && this.currentInput != this.requiredInput) {
-      this.log('setting Input to %s', this.requiredInput);
-      return this.avr.setInput(this.requiredInput);
+  var that = this;
+  
+  this.avr.setPowerState(powerState, function(error, data) {
+    that.log('Sent power state to be %s', data);
+    if(error) {
+      that.log("Error while setting power state %s", error);
     }
-  })
-  .catch((error) => {
-      // Oh noez.
-      this.log('Error %s', error);
-  });
 
-  callback(null);
+    //Get input to check if it matches
+    that.avr.getInput(function(error, input) {
+      if(error) {
+        that.log("Error while getting input state %s", error);
+      }
+
+      if(input != that.requiredInput) {
+        //Switch input
+        that.avr.setInput(that.requiredInput, function(error, data) {
+          that.log('Sent input state to be %s', that.requiredInput);
+
+          if(error) {
+            that.log("Error while setting input state %s", error);
+          }
+          //return after switching input
+          callback(null);
+        });
+      }
+      else {
+        //Return if input already matches
+        callback(null);
+      }
+    });    
+  });
 };
 
 denonClient.prototype.getServices = function () {
