f9983be5a32fbab43046a1c2598b06a49f5b3508
Mart van Vliet <martvvliet@outlook.com>
2019-11-26 22:59:40 +0100

    bugfix

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = function (homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
 
-  homebridge.registerAccessory('homebridge-denon-avr', 'DenonAVR', denonClient);
+  homebridge.registerAccessory('hombridge-denon-heos', 'DenonAVR', denonClient);
 };
 
 function denonClient(log, config) {
